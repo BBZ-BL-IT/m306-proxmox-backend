411c2626be10e44ef004ba19ccf350926708b47d
devjjing <tangjing.gan@bbzbl-it.ch>
2026-03-05 08:10:52 +0100

    feat(api): add sample endpoints for testing

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -6,5 +6,10 @@ mod handlers;
 
 // TODO Add middleware
 pub fn build_router(state: State) -> Router {
-    Router::new().route("/health", get(handlers::health::check_health))
+    Router::new()
+        .route("/health", get(handlers::health::check_health))
+        .route("/api/nodes", get(handlers::proxmox::get_nodes))
+        .route("/api/cluster/status", get(handlers::proxmox::get_cluster_status))
+        .route("/api/cluster/resources", get(handlers::proxmox::get_resources))
+        .with_state(state)
 }
