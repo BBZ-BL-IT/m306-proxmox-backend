16bbf7d112df63121605c379513dd2df53289315
juacamole <juan.luethy@gmail.com>
2026-03-16 10:55:12 +0100

    feat(api): pr feedback improvement

--- src/web/routes/proxmox_routes.rs
+++ src/web/routes/proxmox_routes.rs
@@ -1,18 +1,14 @@
-use axum::{routing::{get, post}, Router};
-use http::header::{CACHE_CONTROL, PRAGMA};
-use http::HeaderValue;
-use tower_http::set_header::SetResponseHeaderLayer;
+use axum::{routing::{get, Router};
 use crate::app_state::AppState;
-use crate::infrastructure::web::handlers::auth_handler{login_handler, logout_handler, me_handler, oauth_callback_handler, refresh_handler};
 
-pub fn auth_routes() -> Router<AppState> {
+pub fn proxmox_routes() -> Router<AppState> {
         Router::new()
-            .route("/config/create",       get())
-            .route("/config/update",       get())
-            .route("/config/storage",       get())
-            .route("/user/list",       get())
-            .route("/group/list",       get())
-            .route("/infrastructure/{vm_id}",       get())
-            .route("/node/list",       get())
-            .route("/vm/list",       get())
+            .route("/config/create", get())
+            .route("/config/update", get())
+            .route("/config/storage", get())
+            .route("/user/list", get())
+            .route("/group/list", get())
+            .route("/infrastructure/{vm_id}", get())
+            .route("/node/list", get())
+            .route("/vm/list", get())
 }
